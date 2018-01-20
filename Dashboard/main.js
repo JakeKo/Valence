dadb21b630f7f3fe5ec429473cd1821334a81124
Jacob Koperski <wijakoperski@gmail.com>
2018-01-20 13:38:11 -0600

    Developed build system file structure

--- Dashboard/main.js
+++ Dashboard/main.js
@@ -2,15 +2,15 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Keep a global reference of the window object.
+// If you don't, the window will close automatically when the JavaScript object is garbage collected.
 let win;
 
 function createWindow () {
 	// Create the browser window.
-	win = new BrowserWindow({width: 800, height: 600});
+	win = new BrowserWindow({width: 1400, height: 1000});
 
-	// and load the index.html of the app.
+	// Load the index.html of the app.
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file:',
